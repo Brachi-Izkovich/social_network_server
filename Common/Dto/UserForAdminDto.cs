3dd7807ceb51a45e3fec7ae78e20404403d27afa
miri118 <mirizinger1@gmail.com>
2025-07-01 21:17:03 +0300

    userdto changed -- userforadminDto added

--- Common/Dto/UserForAdminDto.cs
+++ Common/Dto/UserForAdminDto.cs
@@ -1,20 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Repository.Entities;
 
 namespace Common.Dto
 {
-    public class UserDto
+    public class UserForAdminDto
     {
+        public int Id {  get; set; }
         public string Name { get; set; }
-        public Role Role { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
         public byte[]? ArrImageProfile { get; set; }
         public IFormFile? fileImageProfile { get; set; }
-        public string? ImageProfileUrl { get; set; } 
+        public string? ImageProfileUrl { get; set; }
     }
 }
